c5d3c5f4c96f20a957b0f4152071eb14a9717f8b
geeoneee <jibom0507@naver.com>
2024-11-30 03:00:52 +0900

    Feat: 네비게이션 바 작성

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <NavContainer>
+      <NavMenu>
+        <NavItem>
+          <StyledLink to="/">Pickin'</StyledLink>
+        </NavItem>
+        <NavItem>
+          <StyledLink to="/job-postings">채용 공고</StyledLink>
+        </NavItem>
+        <NavItem>
+          <StyledLink to="/career-guide">취업 가이드</StyledLink>
+        </NavItem>
+        <NavItem>
+          <StyledLink to="/community">커뮤니티</StyledLink>
+        </NavItem>
+      </NavMenu>
+    </NavContainer>
+  );
+};
+
+export default Navbar;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 20px 70px;
+  background-color: #b2efe6;
+`;
+
+const NavMenu = styled.ul`
+  display: flex;
+  gap: 70px;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const NavItem = styled.li``;
+
+const StyledLink = styled(Link)`
+  font-size: 20px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #000;
+
+  &:hover {
+    color: #ffffff;
+  }
+`;
