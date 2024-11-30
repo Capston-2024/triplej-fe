3beb3c5f989c617d9d1fe7107e7db6a8116fea58
geeoneee <jibom0507@naver.com>
2024-12-01 02:39:52 +0900

    Feat: 채용 공고 목록 조회 페이지 작성

--- src/pages/main/Jobpostings.js
+++ src/pages/main/Jobpostings.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Header from "../../components/Header";
 import Navbar from "../../components/Navbar";
 import itImg from "../../assets/itImg.jpg";
+import NaverZ from "../../assets/NaverZ.png";
 import { useState, useEffect } from "react";
 
 const Jobpostings = () => {
@@ -15,6 +16,58 @@ const Jobpostings = () => {
 
     return () => clearInterval(interval);
   }, []);
+
+  const jobPostingsData = [
+    {
+      id: 1,
+      title: "NAVER Z",
+      subTitle: "[NAVER Z] 제페토 글로벌 서비스 운영 체험형 인턴",
+      tags: [
+        "경기",
+        "인턴",
+        "한국어 가능자",
+        "영어",
+        "인도네시아어",
+        "태국어",
+        "F-2",
+        "F-4",
+      ],
+      sustainability: "직무 적합도 90%",
+      imgSrc: NaverZ,
+    },
+    {
+      id: 2,
+      title: "삼성전자",
+      subTitle: "[삼성전자] 글로벌 서비스 운영",
+      tags: ["서울", "정규직", "영어"],
+      sustainability: "직무 적합도 80%",
+      imgSrc: itImg,
+    },
+    {
+      id: 3,
+      title: "LG전자",
+      subTitle: "[LG전자] 글로벌 인턴십 프로그램",
+      tags: ["경기", "인턴", "중국어 가능자"],
+      sustainability: "직무 적합도 85%",
+      imgSrc: itImg,
+    },
+    {
+      id: 4,
+      title: "카카오",
+      subTitle: "[카카오] 글로벌 서비스 운영 팀원 모집",
+      tags: ["제주", "계약직", "일본어", "한국어 가능자"],
+      sustainability: "직무 적합도 75%",
+      imgSrc: itImg,
+    },
+    {
+      id: 5,
+      title: "SK하이닉스",
+      subTitle: "[SK하이닉스] 글로벌 인턴십 모집",
+      tags: ["경상", "인턴", "한국어 가능자", "영어"],
+      sustainability: "직무 적합도 92%",
+      imgSrc: itImg,
+    },
+  ];
 
   return (
     <MainWrapper>
@@ -40,6 +93,30 @@ const Jobpostings = () => {
             </PostBox>
           </Slider>
         </SliderWrapper>
+        <JobPostHeader>
+          <Text>채용 공고</Text>
+          <TabContainer>
+            <TabButton>추천 순</TabButton>
+            <TabButton>내가 찜한 순</TabButton>
+          </TabContainer>
+        </JobPostHeader>
+        <JobPostingsBox>
+          {jobPostingsData.map((job) => (
+            <JobPostBox key={job.id}>
+              <JobImg style={{ backgroundImage: `url(${job.imgSrc})` }} />
+              <JobInfoContainer>
+                <JobTitle>{job.title}</JobTitle>
+                <JobSubTitle>{job.subTitle}</JobSubTitle>
+                <DetailTags>
+                  {job.tags.map((tag, index) => (
+                    <Tag key={index}>{tag}</Tag>
+                  ))}
+                </DetailTags>
+              </JobInfoContainer>
+              <Sustainability>{job.sustainability}</Sustainability>
+            </JobPostBox>
+          ))}
+        </JobPostingsBox>
       </Container>
     </MainWrapper>
   );
@@ -104,4 +181,99 @@ const Finaldate = styled.div`
   font-weight: 900;
   color: #ffffff;
   margin-top: 80px;
+`;
+
+const JobPostHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 60px 10px 20px 10px;
+`;
+
+const Text = styled.div`
+  font-size: 35px;
+  font-weight: 900;
+`;
+
+const TabContainer = styled.div`
+  background-color: #ffffff;
+  padding: 20px 30px;
+  width: 300px;
+  border-radius: 20px;
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+`;
+
+const TabButton = styled.button`
+  font-size: 25px;
+  font-weight: 900;
+  border: none;
+  border-radius: 10px;
+  background-color: #d9d9d9;
+  cursor: pointer;
+  padding: 10px 20px;
+
+  &:hover {
+    background-color: #b2efe6;
+  }
+`;
+
+const JobPostingsBox = styled.div`
+  background-color: #ffffff;
+  padding: 60px 80px;
+`;
+
+const JobPostBox = styled.div`
+  background-color: #d9d9d9;
+  padding: 40px 80px;
+  border-radius: 10px;
+  display: flex;
+  margin-bottom: 30px;
+`;
+
+const JobImg = styled.div`
+  background-image: url(${NaverZ});
+  background-size: cover;
+  background-position: center;
+  height: 200px;
+  width: 300px;
+  flex-shrink: 0;
+  margin-right: 30px;
+`;
+
+const JobInfoContainer = styled.div`
+  width: 800px;
+`;
+
+const JobTitle = styled.div`
+  font-size: 35px;
+  font-weight: 900;
+`;
+
+const JobSubTitle = styled.div`
+  font-size: 25px;
+  font-weight: 900;
+  color: #ffffff;
+`;
+
+const DetailTags = styled.div`
+  display: flex;
+  gap: 8px;
+  margin-top: 8px;
+`;
+
+const Tag = styled.span`
+  background-color: #f0f0f0;
+  padding: 4px 8px;
+  border-radius: 4px;
+
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const Sustainability = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  margin-left: 40px;
 `;
